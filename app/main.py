d3b30936b1b357bf4bdb7592e121ccd8be61a4f0
Dave Campbell <daveccampbell@gmail.com>
2023-09-17 10:35:50 -0400

    added flask and mounted flask app

--- app/main.py
+++ app/main.py
@@ -1,16 +1,30 @@
 # app/main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.wsgi import WSGIMiddleware
+from flask import Flask, render_template
 
 from app.db import database, User
 
-
+# init FastAPI app and Flask app
 app = FastAPI(title="FastAPI, Docker, and Traefik")
+flask_app = Flask(__name__)
 
+# mount Flask on FastAPI
+app.mount("/flask",WSGIMiddleware(flask_app))
+
+
+@flask_app.get('/blog')
+def blog_page():
+  return "Blog Section from Flask"
 
 @app.get("/")
 async def read_root():
     return await User.objects.all()
+
+@app.get("/about")
+async def about_page():
+  return "This is the About Page"
 
 
 @app.on_event("startup")
