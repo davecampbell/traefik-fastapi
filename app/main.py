ea8d3b0e908e59b0afb3158d8fa4029329eacd63
Dave Campbell <daveccampbell@gmail.com>
2023-09-17 22:49:59 -0400

    lots more flasky stuff

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 # app/main.py
 
+import datetime
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.wsgi import WSGIMiddleware
 from flask import Flask, render_template
@@ -18,7 +20,36 @@ app.mount("/flask",WSGIMiddleware(flask_app))
 def blog_page():
   return "Blog Section from Flask"
 
-@app.get("/")
+@flask_app.route('/')
+def hello():
+   return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+@flask_app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@flask_app.route('/comments/')
+def comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.'
+                ]
+
+    return render_template('comments.html', comments=comments)
+
+@flask_app.route('/other_comments/')
+def other_comments():
+    comments = ['This is the very fifth comment.',
+                'This is the very sixth comment.',
+                'This is the very seventh comment.',
+                'This is the very eighth comment.'
+                ]
+
+    return render_template('other_comments.html', comments=comments)
+
+
+@app.get("/fastapi")
 async def read_root():
     return await User.objects.all()
 
